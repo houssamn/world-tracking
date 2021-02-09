e5128a916efdf49ae89992e3e6ebb421bd5f9bb3
Houssam <houssam.naous1@gmail.com>
2021-02-09 21:55:32 +0100

    placed a box relative to origin

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
     }
 
-
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0,0.2,-0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
 }
 
