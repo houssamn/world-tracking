03e12ebbac49c33466b3100af7d59518a5e415c9
Houssam <houssam.naous1@gmail.com>
2021-02-09 22:10:36 +0100

    added specular

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -17,15 +17,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0.2,-0.3)
+        node.position = SCNVector3(0,0,-0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
+    @IBAction func resetButton(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
 }
 
