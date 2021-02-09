a8e9615e63e23b255c5945af06cd7a0facbe8909
Houssam <houssam.naous1@gmail.com>
2021-02-09 21:42:44 +0100

    working template ARKit sceneView

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @IBOutlet weak var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
     }
 
 
